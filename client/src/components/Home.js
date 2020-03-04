097c939a343bbbc07d878c76e17d2ac58e42ea19
NobleTan <noblet@sfu.ca>
2020-03-03 16:14:41 -0800

    update

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+    import React from 'react';
+import './Home.css'
+
+function Home(props) {
+    return (
+        <div>
+             <div>
+            <h1>Toupler</h1>
+            </div>
+            {/* <TopNa onclickhandler={props.onclickhandler}/> */}
+        
+        </div>
+    )
+}
+
+export default Home;
